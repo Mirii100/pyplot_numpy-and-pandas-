03e77337eb725aa1cc1fd9b76e6dd1bf5e986fa3
mirii Alex Njuguna <145835776+Mirii100@users.noreply.github.com>
2024-03-11 17:55:07 +0300

    Add files via upload

---
+++ bargrapg.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+plt.title("matplotlib")
+plt.bar(x,y)
+plt.show()
---
+++ hello.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+lab=np.array([1,11,2,33,444,5,22,34])
+plt.pie(lab)
+plt.title("labb")
+plt.show()
